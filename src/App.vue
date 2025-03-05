5a9db457390de723900002e2a3deb5b812ec90b8
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-03-05 16:51:53 +0800

    basic setting

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex flex-row flex-wrap justify-center leading-8 gap-4">
+    <RouterLink to="/" class="hover:underline">Home</RouterLink>
+    <RouterLink to="/helloworld" class="hover:underline">Hello World</RouterLink>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>
+    <RouterView />
+  </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
