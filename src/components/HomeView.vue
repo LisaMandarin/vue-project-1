e8baae6993087bc130c3578e08f924c369921366
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-03-07 02:17:31 +0800

    feat: home link

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,3 +1,10 @@
 <template>
-    <h1>Home View</h1>
+    <div class="w-sm mx-auto text-center">
+        <h1>Home View</h1>
+        <p>
+            <a href="https://vuejs.org/examples/#hello-world" class="link">
+                learning examples
+            </a>
+        </p>
+    </div>
 </template>
